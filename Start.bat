4127d358ecbc4fc76b7eb95d4711d6a7eb78393a
MokyNZTTV <mokynzmc@gmail.com>
2025-11-27 02:24:07 +1300

    I think the updater works now

--- Start.bat
+++ Start.bat
@@ -1,4 +1,3 @@
 @echo off
-update.bat
 title RoR2 Save Editor
 python .\Code\main.py
--- Update.bat
+++ Update.bat
@@ -1,4 +1,8 @@
 @echo off
 Title Git Updater
+echo You must have git installed and on PATH to use the update script. Continue if this is fine, die if it is not
+pause
 git init
 git pull https://github.com/MokyNZZ/RoR2-Save-Editor
+echo Done updating!
+pause
